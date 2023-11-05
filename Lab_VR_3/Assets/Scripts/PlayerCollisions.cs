4c1a7fc04c4d179fba184664ecaf4d802c5809c8
paula048 <97178187+paula048@users.noreply.github.com>
2023-11-05 10:59:18 +0100

    lab_4_not_everything
    open and close door, by collision

---
+++ Lab_VR_3/Assets/Scripts/PlayerCollisions.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+
+    bool doorIsOpen = false;
+    float doorTimer = 0.0f;
+    public float doorOpenTime = 3.0f;
+    public AudioClip doorOpenSound;
+    public AudioClip doorShutSound;
+
+    GameObject currentDoor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(doorIsOpen){
+            doorTimer += Time.deltaTime;
+            if(doorTimer > doorOpenTime){
+                //ShutDoor(currentDoor);
+                Door(doorShutSound, false, "doorshut", currentDoor);
+                doorTimer = 0.0f;
+            }
+        }
+    }
+
+
+    void OnControllerColliderHit(ControllerColliderHit hit){
+
+        if(hit.gameObject.tag == "playerDoor" && doorIsOpen == false){
+            //OpenDoor(hit.gameObject);
+            currentDoor = hit.gameObject;
+            //OpenDoor(currentDoor);
+            Door(doorOpenSound, true, "dooropen", currentDoor);
+
+        }
+    }
+
+/*
+    void OpenDoor(GameObject door){
+        doorIsOpen = true;
+        door.GetComponent<AudioSource>().PlayOneShot(doorOpenSound);
+        door.transform.parent.GetComponent<Animation>().Play("dooropen");
+    }
+
+
+    void ShutDoor(GameObject door){
+        doorIsOpen = false;
+        door.GetComponent<AudioSource>().PlayOneShot(doorShutSound);
+        door.transform.parent.GetComponent<Animation>().Play("doorshut");
+    }
+*/
+
+
+    void Door(AudioClip aClip, bool openCheck, string animName, GameObject thisDoor){
+        if(openCheck == true){
+            doorIsOpen = true;
+        }else doorIsOpen = false;
+        thisDoor.GetComponent<AudioSource>().PlayOneShot(aClip);
+        thisDoor.transform.parent.GetComponent<Animation>().Play(animName);
+
+    }   
+
+
+
+
+
+
+}
